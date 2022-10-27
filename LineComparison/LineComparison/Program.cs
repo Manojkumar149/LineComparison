59d7a1b0c26ce7262da752d1fd75088a69dcbca0
Manoj Kumar Manukonda <manojkumarmanukonda77@gmail.com>
2022-10-27 17:38:55 +0530

    [Manoj] Add: Calculate Length

--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Problem Statement");
+            CalculateLength calculateLength = new CalculateLength(1,2,3,4);
+            calculateLength.Calculate();
         }
     }
 }
