6407bd6ea003cc387497af3473f3b9f7d089571c
Manoj Kumar Manukonda <manojkumarmanukonda77@gmail.com>
2022-10-28 08:40:06 +0530

    [Manoj] Add: Check equality of 2 length

--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -5,8 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Problem Statement");
-            CalculateLength calculateLength = new CalculateLength(1,2,3,4);
-            calculateLength.Calculate();
+            CalculateLength calculateLength1 = new CalculateLength(1,2,3,4);
+            double length1 = calculateLength1.Calculate();
+            CalculateLength calculateLength2 = new CalculateLength(1, 2, 3, 4);
+            double length2 = calculateLength2.Calculate();
+            if(length1 == length2)
+                Console.WriteLine("length1 is equals to length2");
+            else
+                Console.WriteLine("length1 is not equals to length2");
+
         }
     }
 }
